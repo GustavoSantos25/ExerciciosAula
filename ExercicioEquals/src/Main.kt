f1bb1ee073ea5ad3a0d408fdecd8562c8bffcd96
Camila Pelegrina <pelegrina.camila@gmail.com>
2020-09-18 21:13:22 -0300

    Class Coca

--- ExercicioEquals/src/Main.kt
+++ ExercicioEquals/src/Main.kt
@@ -1,7 +1,12 @@
 fun main(){
     val pessoa1 = Pessoa("Gustavo", 1)
-    val pessoa2 = Pessoa("Camila", 2)
+    val pessoa2 = Pessoa("Camila", 1)
 
     println("Resultado 1: ${pessoa1.equals(pessoa2)}")
+
+    val coca1 = Coca(350, 4.50)
+    val coca2 = Coca(350, 5.30)
+
+    println("Resultado 1: ${coca1.equals(coca2)}")
 
 }
